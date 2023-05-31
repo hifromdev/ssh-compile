c0b76c7e9aa18b8b5ea3e93c3d836dc2578f1801
Dev Patel <hifromdev@gmail.com>
2023-05-31 17:56:26 -0400

    Made file transfer put/get & ssh compile

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# SSH-Compile
+# SSH-Compiler
 
-This is a succint python script that uses SSH to connect to a virtual machine, compile some C++ code, and place the compiled binary back onto the host system. I created this script to allow myself to compile C++ code on newer versions of MacOS due to, what I deem, unsolvable headaches with package conflicts during compile-time.
+This is a succint python script that uses SSH to connect to a virtual machine, compile some C++ code, and place the compiled binary back onto the host system. I created this script to allow C++ compilation on newer versions of MacOS in face of package conflicts during compile-time.
--- main.py
+++ main.py
@@ -3,15 +3,30 @@ import json
 from paramiko.client import SSHClient
 from paramiko import AutoAddPolicy
 
-with open('secrets.json') as user_file:
-  credentials = json.load(user_file)
+# From file_to_compile, its corresponding output file name is generated in file_to_recieve.
+#   Ex: main.cpp generates main.o and main.min.cpp generates main.min.o
+file_to_compile = "main.cpp"
+file_to_recieve = f"{''.join(file_to_compile.split('.')[:-1])}.o"
 
+# Loads virtual machine credentials into dictionary 'credentials'
+with open('secrets.json') as secrets:
+  credentials = json.load(secrets)
+
+# Create and connect to the SSH connection
 client = SSHClient()
 client.set_missing_host_key_policy(AutoAddPolicy())
-
 client.connect(credentials["domain"], username=credentials["username"], password=credentials["password"])
-stdin, stdout, stderr = client.exec_command('ls -l')
 
+###
+# Transfer the file to compile, compile said file, and recieve compiled binary.
+sftp = client.open_sftp()
+sftp.put(file_to_compile, f"/home/{credentials['username']}/{file_to_compile}")
+
+stdin, stdout, stderr = client.exec_command(f'g++ {file_to_compile} -o {file_to_recieve}')
 stdin.close()
-print(stdout.read().decode('utf-8'))
+
+sftp.get(f"/home/{credentials['username']}/{file_to_recieve}", file_to_recieve)
+###
+
+print(stdout.read().decode('utf-8')) # This is not working right now.
 client.close()
